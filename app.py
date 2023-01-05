c57660c24e7f6542397fdcdb1bfbe7e10f80d368
tonglin2003 <tl82003@gmail.com>
2023-01-05 11:09:42 -0500

    A simple change

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Goodbye World!'
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
