15b76564a25dced29cb2f5a324051b39cc466a47
midoriengineer <115385098+midoriengineer@users.noreply.github.com>
2023-01-05 21:09:15 -0500

    .

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():  # put application's code here
-    return render_template("base.html")
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
