8b9b49969acf6a1a3e83c3047a155b116910cfb2
midoriengineer <115385098+midoriengineer@users.noreply.github.com>
2023-01-08 05:45:45 -0500

    react update

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import template from './template';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <>
+      {/* <main-header></main-header> */}
+      <Header />
+      hi
+      <button>ji</button>
+      
+    </>
+    
+
+
+  );
+}
+
+export default App;
