d27b351e8c3f84380631165377ae2752937a9cab
tonglin2003 <tl82003@gmail.com>
2023-01-09 13:22:21 -0500

    Able to use API images in the browsing page

--- react frontend/src/BrowseProducts.js
+++ react frontend/src/BrowseProducts.js
@@ -1,47 +1,88 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Header from './components/Header'
-import Footer from './components/Footer'
-import Spacer from './components/Spacer'
 import Cards from './components/Cards';
 import Search from './components/Search';
+import axios from "axios"
 
+//todo: Fit all the images and titles and price into the cards!!
 
+//todo: Fetch all the infos of the product into json
+
+//todo: display it on the cards somehow...
 function BrowseProducts() {
+
+     const [products, setProduct] = useState([]);
+    const fetchProductInfo = () => {
+        return axios.get("https://fakestoreapi.com/products")
+            .then((response) => setProduct(
+                response.data
+            ));
+    }
+
+    useEffect(() =>{
+        fetchProductInfo();
+    }, [])
+
+    products.map(product => console.log(product['title']))
+    // console.log(`This is the product indexing: ${products}`)
     return (
         <>
             <Header />
-            <Spacer />
+            <br></br> <br></br>
 
             <h1 style={{marginLeft: '50px'}}>Browse Products</h1>
-            
+
             <div style={{marginLeft: '50px'}} >
                 <Search />
             </div>
 
-            <Spacer />
+            <p><br></br> <br></br></p> <p></p>
 
             <div className="row row-cols-3 g-3" style={{marginLeft: '50px', marginRight: '50px' }}>
 
                 {/* Small Cards */}
-                <Cards size={1} image={1} />
-                <Cards size={1} image={2} />
-                <Cards size={1} image={3} />
-                <Cards size={1} image={4} />
+                {/*<Cards size={1} image={"https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp"} />*/}
+                {/*<Cards size={1} image={2} />*/}
+                {/*<Cards size={1} image={3} />*/}
+                {/*<Cards size={1} image={4} />*/}
+
+                {
+                    products.map(product => (
+                        <Cards size={1} image={product['image']} />
+                    ))
+                }
+
+{/*                {(typeof products === 'undefined') ? (*/}
+{/*    <p>loading...</p>*/}
+{/*) : (*/}
+{/*    products.map(item => (*/}
+{/*            <div className={"m-5"}>*/}
+{/*                    <a href={"/"}>{item}</a>*/}
+{/*                    <img src={"https://static.swappa.com/static/images/categories/retro/category_iphones_600x600.png"} className={"img-thumbnail"}/>*/}
+
+{/*            </div>*/}
+{/*        )*/}
+{/*    ))}*/}
+
+
 
             </div>
-            <Spacer />
 
-               <div className="row row-cols-3 g-3" style={{marginLeft: '50px', marginRight: '50px' }}>
 
-                {/* Small Cards */}
-                <Cards size={1} image={1} />
-                <Cards size={1} image={2} />
-                <Cards size={1} image={3} />
-                <Cards size={1} image={4} />
+            {/*<p><br></br> <br></br></p> <p></p>*/}
 
-            </div>
-            <Spacer />
-            <Footer />
+            {/*   <div className="row row-cols-3 g-3" style={{marginLeft: '50px', marginRight: '50px' }}>*/}
+
+            {/*    /!* Small Cards *!/*/}
+            {/*    <Cards size={1} image={1} />*/}
+            {/*    <Cards size={1} image={2} />*/}
+            {/*    <Cards size={1} image={3} />*/}
+            {/*    <Cards size={1} image={4} />*/}
+
+            {/*</div>*/}
+            <p><br></br> <br></br></p> <p></p>
+
+
         </>
     )
 }
--- react frontend/src/components/Cards.js
+++ react frontend/src/components/Cards.js
@@ -2,11 +2,29 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 var image = "";
+// const Cards = ({size, image}) => {
+//
+//
+//
+//     if(image===1){image ='https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp'}
+//     else if(image===2){image ='https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp'}
+//     else if(image===3){image ='https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp'}
+//     else if(image===4){image ='https://mdbcdn.b-cdn.net/img/new/standard/city/050.webp'}
+//     console.log(image);
+//
+//     if(size === 1){ return <SmallCard image = {image} /> }
+//     else if(size === 2){ return <MediumCard image = {image} />}
+//     else if (size === 3) {return <LargeCard image ={image} /> }
+// }
+
 const Cards = ({size, image}) => {
-    if(image===1){image ='https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp'}
-    else if(image===2){image ='https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp'}
-    else if(image===3){image ='https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp'}
-    else if(image===4){image ='https://mdbcdn.b-cdn.net/img/new/standard/city/050.webp'}
+
+    // if(image===1){image ='https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp'}
+    // else if(image===2){image ='https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp'}
+    // else if(image===3){image ='https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp'}
+    // else if(image===4){image ='https://mdbcdn.b-cdn.net/img/new/standard/city/050.webp'}
+    if(image){image =`${image}`}
+    // console.log(image);
 
     if(size === 1){ return <SmallCard image = {image} /> }
     else if(size === 2){ return <MediumCard image = {image} />}
@@ -14,34 +32,29 @@ const Cards = ({size, image}) => {
 }
 
 const LargeCard = ({image}) => {
+    console.log(image);
     return(
     <>
-        <div className="col text-center">
-            <div className="col text-center">
+        <div class="col text-center">
+            <div class="col text-center">
                 <h1>Write the product / subsciption name here</h1>
                 <p> <br></br></p>
-                <div className="col d-flex justify-content-center">
-                    <div className="card">
-                    </div>
-                    <div className="card" style={{width: '45 rem'}}>
-                        <img src={image} className="card-img-top" alt="Hollywood Sign on The Hill" />
-                        <div className="card-body">
-                            <h5 className="card-title">
-                                Card title
-                            </h5>
-                            <p className="card-text">
-                                Write samething as it was in the last page
-                            </p>
+                <div class="col d-flex justify-content-center"><div class="card"></div>.
+                    <div class="card" style={{width: '45 rem'}}>
+                        <img src={image} class="card-img-top" alt="Hollywood Sign on The Hill" />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Write samething as it was in the last page</p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div className="col text-center">
+        <div class="col text-center">
 
             <p> <br></br> </p>
-            <p> Write description of product. for example:</p>
+            <p> Write discrption of product. for example:</p>
 
             <li>
                 <ul id="center">Imported</ul>
@@ -50,6 +63,7 @@ const LargeCard = ({image}) => {
             </li>
             <p><br></br></p>
             check out more products below
+            <p><br></br></p>
         </div>
         </>
     )
@@ -58,16 +72,16 @@ const LargeCard = ({image}) => {
 const MediumCard = ({image}) => {
     return(
         <>
-            <div className="col-sm">
-                <div className="card">
-                    <img src={image} className="card-img-top" alt="Palm Springs Road" />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">
+            <div class="col-sm">
+                <div class="card">
+                    <img src={image} class="card-img-top" alt="Palm Springs Road" />
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">
                             This is a longer card with supporting text below as a natural lead-in to
                             additional content. This content is a little bit longer.
                         </p>
-                        <div className="col text-center">
+                        <div class="col text-center">
                             <Link to="/" class="btn btn-primary">Go somewhere</Link>
                         </div>
                     </div>
@@ -100,4 +114,3 @@ const SmallCard = ({image}) => {
 }
 
 export default Cards
-
