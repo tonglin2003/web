519a7c934b32a69a299f36993d5c427b03af568c
tonglin2003 <tl82003@gmail.com>
2023-01-10 16:21:24 -0500

    Updated home page design

--- react frontend/src/Home.js
+++ react frontend/src/Home.js
@@ -4,8 +4,15 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Spacer from './components/Spacer'
 import './css/styles.css'
+import Carousel from 'react-bootstrap/Carousel';
+import CarouselItem from "./components/Category_Cards";
+import RecommendDisplay from "./components/recommendDisplay"
+
+
+
 
 function Home() {
+
     return (
         <>
             <Header />
@@ -32,8 +39,7 @@ function Home() {
                             and access to corporate-level benefits that ensure people feel secure and valued.
                         </h4>
                         <p className="font fw-bold m-3">Find your special discount offer here: </p>
-                        <Link to="/browse" style={{textDecoration: "none"}}><button type="button" className="button-6 m-3">Start digging your treasure!</button></Link>
-
+                        <Link to="/browse" style={{textDecoration: "none"}}><button type="button" className="button-6 m-3">Find Discount</button></Link>
                     </div>
                     <div className="col">
                         <img src={ "https://assets-global.website-files.com/5e19ea5aa7d3a217492e372b/61786221a8b701a33c2b9f0d_AxiosHQ_Launch_house-ad%20(1).png" }
@@ -44,7 +50,42 @@ function Home() {
                     </div>
                 </div>
             </div>
-            
+
+            <Spacer/>
+
+    <div className={"py-5 my-5"} style={{borderBottom: "0.1vh #9facbf solid", borderTop: "0.1vh #9facbf solid", background: "#ebedf0"}}>
+        <Carousel variant={"dark"}>
+            {/*<CarouselItem  image={"https://media.istockphoto.com/id/916092484/photo/women-clothes-hanging-on-hangers-clothing-rails-fashion-design.jpg?s=612x612&w=0&k=20&c=fUpcbOITkQqitglZfgJkWO3py-jsbuhc8eZfb4sdrfE="} category={"Something"}/>*/}
+            <CarouselItem image={"https://www.classcentral.com/report/wp-content/uploads/2021/06/udemy-sidebar.jpg"} category={"Online Courses"}/>
+        </Carousel>
+    </div>
+
+    <div className="container">
+        <div className="row">
+            <RecommendDisplay num={0}
+                              image={"https://i.insider.com/60a2c77ef27b4c0019444ece?width=1200&format=jpeg"}
+                              description={"Find the best offer only at Justworks for Women Clothing."}
+                              category={"Womon's Clothing"}
+                              link={"/browse"}
+            />
+            <RecommendDisplay num={1}
+                              image={"https://www.werd.com/wp-content/uploads/2022/08/taylor-stitch.jpg"}
+                              description={"Today is the day to buy some Man Clothing with the best offer at Justworks."}
+                              category={"Man's Clothing"}
+                              link={"/browse"}
+            />
+            <RecommendDisplay num={2}
+                              image={"https://kinsta.com/wp-content/uploads/2022/11/kinsta-cloud-hosting.jpg"}
+                              description={"Technology Tool could be convenience and cheaper with the offers provided at Justworks."}
+                              category={"Technology Tool"}
+                              link={"/browse"}
+            />
+
+
+        </div>
+    </div>
+
+
             <Spacer />
             <Footer />
 
