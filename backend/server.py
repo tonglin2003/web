c3126b2f17c3ae6b320f37ad73cc496ac1df7e49
midoriengineer <115385098+midoriengineer@users.noreply.github.com>
2023-01-10 12:15:13 -0500

    .

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 # API Route
+@app.route('/get', methods = ['GET'])
+def get_articles():
+    return jsonify({"Hello":"World"})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
