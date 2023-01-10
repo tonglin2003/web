951a837c64c6ac0bfeeb01a8000cb7f648b87cb2
rcyren <122037204+rcyren@users.noreply.github.com>
2023-01-10 13:41:15 -0500

    adding columns

--- react frontend/src/BriefBio.js
+++ react frontend/src/BriefBio.js
@@ -51,6 +51,21 @@ function BriefBio() {
           </div>
         </div>
       </div>
+
+
+<div class="container">
+  <div class="row">
+    <div class="col align-self-start">
+      <img src="https://i.ibb.co/Wsr9wjX/Pexels-photo-1681010.jpg"/>
+    </div>
+    <div class="col align-self-center">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu volutpat odio facilisis mauris sit amet. Orci a scelerisque purus semper eget. Non sodales neque sodales ut. Tellus at urna condimentum mattis pellentesque id nibh tortor. Tellus mauris a diam maecenas. Phasellus vestibulum lorem sed risus ultricies tristique. Sit amet porttitor eget dolor morbi non arcu risus quis. Volutpat est velit egestas dui. Non nisi est sit amet facilisis magna etiam tempor orci. Mauris a diam maecenas sed enim ut sem viverra. Nec nam aliquam sem et tortor consequat id. Egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Et malesuada fames ac turpis egestas integer. In est ante in nibh mauris. Adipiscing at in tellus integer feugiat. Gravida rutrum quisque non tellus orci ac auctor. In eu mi bibendum neque egestas congue quisque. Neque aliquam vestibulum morbi blandit cursus risus at ultrices.
+    </div>
+    <div class="col align-self-end">
+     <img src="https://i.ibb.co/7SxWmpX/Pexels-photo-14917591.jpg"/>
+    </div>
+  </div>
+</div>
       </>
     )
   }
