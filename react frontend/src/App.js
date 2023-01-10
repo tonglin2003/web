cbe8ebe1456b7cf9fb324ed555ea8db483ceca12
rcyren <122037204+rcyren@users.noreply.github.com>
2023-01-10 11:54:53 -0500

    BriefBio-Test

--- react frontend/src/App.js
+++ react frontend/src/App.js
@@ -3,6 +3,7 @@ import Home from './Home'
 import CompanyBio from './CompanyBio'
 import PostPage from './PostPage'
 import SingleProduct from './SingleProduct'
+import BriefBio from './BriefBio'
 import BrowseProducts from './BrowseProducts'
 
 
@@ -14,6 +15,7 @@ function App() {
       <Route exact path="/bio" element={<CompanyBio />} />
       <Route exact path="/post" element={<PostPage />} />
       <Route exact path="/product" element={<SingleProduct />} />
+      <Route exact path="briefbio" element={<BriefBio />} />
       <Route exact path="/browse" element={<BrowseProducts />} />
 
     </Routes>    
---
+++ react frontend/src/BriefBio.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+
+function BriefBio() {
+    return (
+        <>
+        <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">Brief Client Bio</h1>
+          <img class="img" src="https://i.ibb.co/FsZ7g03/Pexels-photo-269077.jpg"/>
+          <p class="lead">
+      Information about the client and services, who they are what they provide. 
+      </p>
+        </div>
+      </div>
+      
+      <div class="card text-center">
+        <div class="card-header">
+          Connections Made Possible by Justworks
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Name of Client</h5>
+          <p class="card-text">Their slogan etc. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu volutpat odio facilisis mauris sit amet. Orci a scelerisque purus semper eget. Non sodales neque sodales ut. Tellus at urna condimentum mattis pellentesque id nibh tortor. Tellus mauris a diam maecenas. Phasellus vestibulum lorem sed risus ultricies tristique. Sit amet porttitor eget dolor morbi non arcu risus quis. Volutpat est velit egestas dui. Non nisi est sit amet facilisis magna etiam tempor orci. Mauris a diam maecenas sed enim ut sem viverra. Nec nam aliquam sem et tortor consequat id. Egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Et malesuada fames ac turpis egestas integer. In est ante in nibh mauris. Adipiscing at in tellus integer feugiat. Gravida rutrum quisque non tellus orci ac auctor. In eu mi bibendum neque egestas congue quisque. Neque aliquam vestibulum morbi blandit cursus risus at ultrices.</p>
+          <a href="#" class="btn btn-primary">Link to full profile</a>
+        </div>
+        <div class="card-footer text-muted">
+        Founded in 2022
+        </div>
+      </div>
+        
+      <div class="row">
+        <div class="col-sm-6">
+          <div class="card">
+            
+            <div class="card-body">
+              <h5 class="card-title">Jane Doe</h5>
+              <img class="img" src="https://i.ibb.co/6XTFThK/Screen-Shot-2023-01-10-at-9-52-56-AM.png" alt="Card image cap"></img>
+              <p class="card-text">"Personal quote"</p>
+              <a href="#" class="btn btn-primary">Learn More</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">John Doe</h5>
+              <img class="img" src="https://i.ibb.co/zZ0Yy1N/Screen-Shot-2023-01-10-at-10-16-03-AM.png" alt="Card image cap"></img>
+              <p class="card-text">Personal Quote</p>
+              <a href="#" class="btn btn-primary">Learn More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      </>
+    )
+  }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<BriefBio />);
+            
+export default BriefBio;
