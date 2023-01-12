e87ee602c626302d320471490293966e93cab9e1
midoriengineer <115385098+midoriengineer@users.noreply.github.com>
2023-01-11 20:28:54 -0500

    Login Data Creation

--- react frontend/src/App.js
+++ react frontend/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './Home'
 import CompanyBio from './CompanyBio'
--- react frontend/src/Login.js
+++ react frontend/src/Login.js
@@ -1,15 +1,65 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Spacer from './components/Spacer'
 import css from './css/styles.css'
 
-
-
 function Login() {
+
+    //Default current user is none (LOGGED OUT)------------------------------------
+    const [currentUser, setCurrentUser] = useState("");
+    useEffect(() => {
+        localStorage.setItem('currentUser', JSON.stringify(currentUser));
+    }, [currentUser]);
+
+    //Setting username = current user (LOGGED IN)------------------------------------
+    const [username, setUsername] = useState();
+    useEffect(() => {
+        const username = JSON.parse(localStorage.getItem('currentUser'));
+        if(username){
+            setUsername(username);
+        }
+    }, []);
+
+    //Retrieve user data from the backend ------------------------------------
+    const [userData, setUserData] = useState()
+    useEffect(() => {
+        fetch("/userdata")
+        .then((res) => res.json())
+        .then((userData) => {
+            setUserData(userData);
+            setLoading(false);
+        });
+    }, []);
+    
+    //DEBUG USER DATA (START)-----------------
+    const [loading, setLoading] = useState(true);
+
+    console.log(userData);
+    if(loading && !userData){
+        return <p>Loading...</p>;
+    }
+    if(!loading && !userData){
+        return <p>Opps. Something went wrong</p>;
+    }
+    //DEBUG USER DATA (END)-----------------
+
+    //------------------------------------------------------------------------
+    
+    var currentUserData = userData[1]
+
     return (
         <>
             <Header />
+
+            <div>
+                {currentUserData.name}
+                {userData.map((user, i) => (
+                    <h3 key={i}>{user.name}</h3>
+                ))}
+            </div>
+       
+       
             <div className='profile background'>
             <Spacer />
             <div  style={{justifyContent: "center", display: "flex"}}>
