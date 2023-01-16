666cf3bb313c288792d4bf8029d0a7833cf8d722
midoriengineer <115385098+midoriengineer@users.noreply.github.com>
2023-01-16 15:27:38 -0500

    cleaning

--- react frontend/src/Dashboard.js
+++ react frontend/src/Dashboard.js
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from "react";
 import {useNavigate} from 'react-router-dom'
 import Header from './components/WebHeaderAndFooter/Header'
 import Footer from './components/WebHeaderAndFooter/Footer'
-import Sidebar from './components/DashSidebar'
 import { NavLink } from 'react-router-dom';
 import {
   CDBSidebar,
