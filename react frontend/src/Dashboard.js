ebe3d7c54110b19644a3f0df1f7022467e6e969d
midoriengineer <115385098+midoriengineer@users.noreply.github.com>
2023-01-17 21:55:31 -0500

    connect feature start

--- react frontend/src/Dashboard.js
+++ react frontend/src/Dashboard.js
@@ -22,20 +22,30 @@ import css from './css/styles.css'
 const Sidebar = ({ setIsDashboard, setIsAnalytics, setIsAccount}) => {
 
     function LoadAnalytics(){
-            setIsDashboard(false);
-            setIsAccount(false);
-            setIsAnalytics(true);
+        setIsDashboard(false);
+        setIsAccount(false);
+        // setIsConnect(false);
+        setIsAnalytics(true);
     }
     function LoadDashboard(){
-            setIsAnalytics(false);
-            setIsAccount(false);
-            setIsDashboard(true);
+        setIsAnalytics(false);
+        setIsAccount(false);
+        // setIsConnect(false);
+        setIsDashboard(true);
     }
     function LoadAccount(){
         setIsDashboard(false);
         setIsAnalytics(false);
+        // setIsConnect(false);
         setIsAccount(true);
     }
+
+    function LoadConnect(){
+        setIsDashboard(false);
+        setIsAccount(false);
+        setIsAnalytics(false);
+        // setIsConnect(true);
+}
 
     return (
         <div style={{ display: 'flex', overflow: 'scroll initial' }}>
