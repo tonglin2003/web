b4f01a3585c9fe9668d2e6cef68d69729f91e427
tonglin2003 <tl82003@gmail.com>
2023-01-18 14:13:10 -0500

    Every page will scroll to the top of the page

--- react frontend/src/App.js
+++ react frontend/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import React, {Fragment} from 'react'
+import {Routes, Route, BrowserRouter as Router, useLocation} from 'react-router-dom';
 import Home from './Home'
 import Profile from './Profile'
 import PostPage from './PostPage'
@@ -11,27 +11,38 @@ import SignUp from './SignUp'
 import Dashboard from './Dashboard'
 import CategorizeProductPage from "./CategorizeProductPage";
 import Policy from "./Policy";
+import {useLayoutEffect} from 'react';
+
 
 function App() {
+
+  const ScrollToTop = ({children}) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children
+  }
+
   return (
-    <>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/get-started" element={<SignUp />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/profile/user/:userID" element={<Profile />}/>
-      <Route path="/profile" element={<Profile />}/>
-      <Route exact path="/post" element={<PostPage />} />
-      {/*  ":product_id" can be captured in a variable (more details in "SingleProducts.js") */}
-      <Route exact path="/product/:category/:product_id" element={<SingleProduct />} />
-      <Route exact path="/briefbio" element={<BriefBio />} />
-      <Route exact path="/browse" element={<BrowseProducts />} />
-      <Route exact path="/browse/categories/:category" element={<CategorizeProductPage />} />
-      <Route exact path="/policy" element={<Policy/> } />
-      <Route path="/dashboard" element={<Dashboard/>} />
-    </Routes>  
-    </>  
+    <ScrollToTop>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/get-started" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile/user/:userID" element={<Profile />}/>
+        <Route path="/profile" element={<Profile />}/>
+        <Route exact path="/post" element={<PostPage />} />
+        {/*  ":product_id" can be captured in a variable (more details in "SingleProducts.js") */}
+        <Route exact path="/product/:category/:product_id" element={<SingleProduct />} />
+        <Route exact path="/briefbio" element={<BriefBio />} />
+        <Route exact path="/browse" element={<BrowseProducts />} />
+        <Route exact path="/browse/categories/:category" element={<CategorizeProductPage />} />
+        <Route exact path="/policy" element={<Policy/> } />
+        <Route path="/dashboard" element={<Dashboard/>} />
+      </Routes>
+    </ScrollToTop>
   );
 }
 
